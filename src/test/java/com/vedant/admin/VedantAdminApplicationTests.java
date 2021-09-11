09e32e5a5d022d0189ecc81e681a13afcc15ceb9
Abhishek Kumar <ak330569@gmail.com>
2021-09-11 12:53:02 +0530

    src folder added

---
+++ src/test/java/com/vedant/admin/VedantAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vedant.admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VedantAdminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
